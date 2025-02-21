1b885c9a4093f5ba4a43c7cc73a6b8b87bbe2379
Avi <gibraltar.av@gmail.com>
2025-02-21 20:54:24 +0800

    Clean up project and add more memes to seed.sql

--- seed.sql
+++ seed.sql
@@ -11,9 +11,14 @@ INSERT INTO `Orders` VALUES
 (NULL, 1, 41.92, '2025-02-11T11:23:44');
 
 INSERT INTO `Memes` VALUES
-(NULL, 'Crying Cat', 1, 3.99, 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/cat-memes/sticker_2.png'),
-(NULL, 'Polite Cat', 1, 2.99, 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/cat-memes/sticker_1.png'),
-(NULL, 'Confused Cat', 1, 5.99, 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/cat-memes/sticker_8.png');
+(NULL, 'Crying Cat', 1, 3.99, 'static/asset/sadCat.png'),
+(NULL, 'Polite Cat', 1, 2.99, 'static/asset/politeCat.png'),
+(NULL, 'Shocked Cat', 1, 5.99, 'static/asset/shockedCat.png'),
+(NULL, 'Banana Cat', 1, 6.23, 'static/asset/bananaCryingCat.png'),
+(NULL, 'Happy Cat', 1, 1.99, 'static/asset/happyCat.gif'),
+(NULL, 'Judging Cat', 1, 4.44, 'static/asset/judgingCat.gif'),
+(NULL, 'OIAUIA Cat', 1, 7.01, 'static/asset/oiauiaCat.gif'),
+(NULL, 'Angry Cat', 1, 9.00, 'static/asset/angryCat.png');
 
 INSERT INTO `OrderItem` VALUES 
 (NULL, 1, 1, 3),
